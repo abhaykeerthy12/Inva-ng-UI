2326745e57467c7cab009d82703aad7520598b27
abhaykeerthy12 <abhaykeerthy12@gmail.com>
2019-11-21 12:49:46 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { UserComponent } from './core/user/user.component';
+import { AdminComponent } from './core/admin/admin.component';
+import { HomeComponent } from './core/home/home.component';
+import { PagesComponent } from './core/pages/pages.component';
+import { LoginComponent } from './core/user/login/login.component';
+import { RegisterComponent } from './core/user/register/register.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    UserComponent,
+    AdminComponent,
+    HomeComponent,
+    PagesComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
